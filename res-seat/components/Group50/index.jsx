afa68e2b4e5a4969df06b3ffc47809adb5b3c4d8
Seoyoung Ko <63905640+syko0531@users.noreply.github.com>
2022-05-20 09:53:11 +0900

    Add files via upload

---
+++ res-seat/components/Group50/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Group50.css";
+
+function Group50() {
+  return (
+    <div className="group-50">
+      <div className="overlap-group3-7 border-1px-mist-gray">
+        <div className="rating-2 inter-normal-spicy-pink-14px">Rating</div>
+      </div>
+    </div>
+  );
+}
+
+export default Group50;
