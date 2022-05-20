f8e8c0dc9c5c0a2f1c52fbad9be3201db7611715
Seoyoung Ko <63905640+syko0531@users.noreply.github.com>
2022-05-20 09:58:17 +0900

    Add files via upload

---
+++ res-seat/components/Group262/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Group262.css";
+
+function Group262(props) {
+  const { text4 } = props;
+
+  return (
+    <div className="group-25">
+      <div className="overlap-group3-4">
+        <div className="add-to-cart-2 inter-extra-bold-white-20px">ADD&nbsp;&nbsp;TO CART</div>
+        <div className="text-5 inter-semi-bold-white-16px">{text4}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Group262;
