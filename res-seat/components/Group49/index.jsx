3f8aeb4b7e27db1b1bce0ee16e363137c2237080
Seoyoung Ko <63905640+syko0531@users.noreply.github.com>
2022-05-20 09:51:40 +0900

    Add files via upload

---
+++ res-seat/components/Group49/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Group49.css";
+
+function Group49() {
+  return (
+    <div className="group-49">
+      <div className="overlap-group2-5 border-1px-mist-gray">
+        <div className="seats-2 inter-normal-spicy-pink-14px">Seats</div>
+      </div>
+    </div>
+  );
+}
+
+export default Group49;
