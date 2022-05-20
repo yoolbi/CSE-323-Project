41ca8b576e375822f0f5d1bb8aaef2380599130c
Seoyoung Ko <63905640+syko0531@users.noreply.github.com>
2022-05-20 09:57:07 +0900

    Add files via upload

---
+++ res-seat/components/Group61/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Group61.css";
+
+function Group61() {
+  return (
+    <div className="group-61-1">
+      <div className="overlap-group9-10 border-1px-silver">
+        <img className="vector-49" src="/img/vector-38@2x.png" />
+        <div className="photo-05-1 applesdgothicneob00-regular-normal-fuscous-gray-13px">Photo 0/5</div>
+      </div>
+    </div>
+  );
+}
+
+export default Group61;
