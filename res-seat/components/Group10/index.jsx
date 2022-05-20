e43b599895b1569328d31a44c56536d941e5a1f7
Seoyoung Ko <63905640+syko0531@users.noreply.github.com>
2022-05-20 09:42:38 +0900

    Add files via upload

---
+++ res-seat/components/Group10/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Group10.css";
+
+function Group10(props) {
+  const { children } = props;
+
+  return (
+    <div className="group-10">
+      <div className="overlap-group-48">
+        <div className="group-9"></div>
+        <div className="number-19 applesdgothicneob00-regular-normal-black-23px">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Group10;
